95b3dffa82909ee1727e83b20d4dccf37adbdab2
Piyushkumar Rajput <rajput9piyush@users.noreply.github.com>
2021-08-08 16:00:54 +0530

    Creating getState Method

---
+++ redux.js
@@ -0,0 +1,25 @@
+// Redux Library
+const createStore = reducer => {
+    // The createStore should have four parts
+    // 1. state
+    // 2. Get the current state (getState)
+    // 3. Listen to changes on the state (subscribe)
+    // 4. Update the state (dispatch)
+
+    let state;
+    let listeners = [];
+
+    const getState = () => state;
+
+    const subscribe = (listener)=>{
+        listeners.push(listener)
+        return ()=>{
+            listeners = listeners.filter((l) => l !== listener);
+        }
+    };
+    return {
+        getState,
+        subscribe,
+        dispatch
+    }
+}
